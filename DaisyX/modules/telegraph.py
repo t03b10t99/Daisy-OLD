9284847baf7afc198fa219913a587002263173e6
t03b10t99 <82961743+t03b10t99@users.noreply.github.com>
2021-07-10 13:50:41 +0700

    Update telegraph.py

--- DaisyX/modules/telegraph.py
+++ DaisyX/modules/telegraph.py
@@ -77,12 +77,3 @@ async def _(event):
 def resize_image(image):
     im = Image.open(image)
     im.save(image, "PNG")
-
-
-__help__ = """
-I can upload files to Telegraph
- ❍ /tm :Get Telegraph Link Of Replied Media
- ❍ /txt :Get Telegraph Link of Replied Text
-"""
-
-__mod_name__ = "Telegraph"
